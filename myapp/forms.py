5e9062f99e8c67e1218dbc633892558951622ccf
Munish <munishmahey2@gmail.com>
2020-03-26 04:52:53 -0400

    Changes done to registeration page

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from myapp.models import Admin, Worker, Client
 from django.contrib.auth.forms import UserCreationForm
@@ -7,3 +8,4 @@ class CreatUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email', 'first_name','last_name','password1', 'password2']
+
