f35d52beeb968cceaaa3747d5c52971cf681d1c7
Munish <munishmahey2@gmail.com>
2020-03-26 07:04:07 -0400

    registrations css changed

--- myapp/forms.py
+++ myapp/forms.py
@@ -8,4 +8,19 @@ class CreatUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email', 'first_name','last_name','password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username *','class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email *','class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name *','class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name *','class': 'form-control'}),
+        }
+    def __init__(self, *args, **kwargs):
+        super(CreatUserForm, self).__init__(*args, **kwargs)
 
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder': 'Password *'})
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder': 'Confirm Password *'})
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
